e13b86dcca15f5c7bfcd14f98c65e2d19c35624b
maxlim <max@virtueware.com>
2022-05-25 11:07:35 -0700

    Added second legend for size and changed font to Times New Roman.

--- scripts/analysis/electrode_plot.py
+++ scripts/analysis/electrode_plot.py
@@ -2,7 +2,9 @@ from extract_mat import MatExtractor
 from collections import OrderedDict
 import numpy as np
 import matplotlib.pyplot as plt
+plt.rcParams["font.family"] = "Times New Roman"
 import matplotlib.axes._axes as axes
+import matplotlib.figure as figure
 
 
 def get_electrode_data(mat, size_scale=1):
@@ -47,19 +49,13 @@ def get_electrode_data(mat, size_scale=1):
 
 def main():
     # Directory of .mat files
-    path_data = "/Users/maxlim/KosikLab/data/maxone/"
-
-    path_tj = path_data + "tj/"
-    path_mx = path_data + "mx/"
-
-    # .mat files
-    rec_num = 2950
-    path_mat_tj = path_tj + f"{rec_num}_sorted.mat"
-    path_mat_mx = path_mx + f"{rec_num}_sorted.mat"
-
+    data_path = "/Users/maxlim/KosikLab/data/maxone/"
     # MatExtractors
-    mat_tj = MatExtractor(path_mat_tj)
-    mat_mx = MatExtractor(path_mat_mx)
+    rec_num = 2953
+    # mat_tj = MatExtractor(data_path + f"tj/{rec_num}_sorted.mat")
+    # mat_mx = MatExtractor(data_path + f"mx/{rec_num}_sorted.mat")
+    mat_tj = MatExtractor("/Users/maxlim/KosikLab/scripts/good_experiment1_baseline_sorted.mat")
+    mat_mx = MatExtractor("/Users/maxlim/KosikLab/scripts/experiment1_baseline_sorted.mat")
 
     # Plotting Electrodes
     size_scale = 1
@@ -67,43 +63,51 @@ def main():
     electrode_data_mx = get_electrode_data(mat_mx, size_scale=size_scale)
 
     alpha = 0.7
-    fig, axs = plt.subplots(2, 2)
+    fig, ((ax00, ax01), (ax10, ax11)) = plt.subplots(2, 2)   # type:figure.Figure, ((axes.Axes, axes.Axes), (axes.Axes, axes.Axes))
     fig.suptitle(f"Electrode Plots of {rec_num}")
-    fig.supxlabel("x")
-    fig.supylabel("y")
+    fig.supxlabel("x location (μm)")
+    fig.supylabel("y location (μm)")
     fig.canvas.manager.set_window_title(f"{rec_num}_electrode_plots")
 
     locations_all = (*electrode_data_tj[0], *electrode_data_tj[1], *electrode_data_mx[0], *electrode_data_mx[1])
     lim_max = max(locations_all)
-    for ax in axs:
-        for a in ax:  # type:axes.Axes
+    for ax in ((ax00, ax01), (ax10, ax11)):
+        for a in ax:
             a.set_aspect("equal")
             a.set_xlim(0, lim_max)
             a.set_ylim(0, lim_max)
 
-    scatter_mx = axs[0, 0].scatter(*electrode_data_mx, c="#0000FF", alpha=alpha, label="Max")
-    scatter_tj = axs[0, 0].scatter(*electrode_data_tj, c="#FF0000", alpha=alpha, label="TJ")
-    axs[0, 0].set_title("Comparison")
-    legend = axs[0, 0].legend((scatter_tj, scatter_mx),
+    ax00.set_title("Comparison")
+    scatter_mx = ax00.scatter(*electrode_data_mx, c="#0000FF", alpha=alpha, label="Max")
+    scatter_tj = ax00.scatter(*electrode_data_tj, c="#FF0000", alpha=alpha, label="TJ")
+    legend_color = ax00.legend((scatter_tj, scatter_mx), ("TJ", "MX"), loc="upper left")
+    for i in range(len(legend_color.legendHandles)):
+        legend_color.legendHandles[i]._sizes = [100]
+
+    loc_off_screen = (-1000, -1000)
+    # size100 = ax00.scatter(*loc_off_screen, size_scale * 100, c="#000000")
+    size1000 = ax00.scatter(*loc_off_screen, size_scale * 1000, c="#000000")
+    # size2000 = ax00.scatter(*loc_off_screen, size_scale * 2000, c="#000000")
+    legend_sizes = ax00.legend((size1000,),
+                               ("1000 units",),
+                               loc="upper right")
+
+    ax00.add_artist(legend_color)
+    ax00.add_artist(legend_sizes)
+    scatter_tj = ax10.scatter(*electrode_data_tj, c="#FF0000", alpha=alpha, label="TJ")
+    scatter_mx = ax10.scatter(*electrode_data_mx, c="#0000FF", alpha=alpha, label="Max")
+    ax10.set_title("Comparison")
+    legend = ax10.legend((scatter_tj, scatter_mx),
                ("TJ", "MX"),
-               loc="lower right")
+               loc="upper left")
     for i in range(len(legend.legendHandles)):
         legend.legendHandles[i]._sizes = [100]
 
-    scatter_tj = axs[1, 0].scatter(*electrode_data_tj, c="#FF0000", alpha=alpha, label="TJ")
-    scatter_mx = axs[1, 0].scatter(*electrode_data_mx, c="#0000FF", alpha=alpha, label="Max")
-    axs[1, 0].set_title("Comparison")
-    legend = axs[1, 0].legend((scatter_tj, scatter_mx),
-               ("TJ", "MX"),
-               loc="lower right")
-    for i in range(len(legend.legendHandles)):
-        legend.legendHandles[i]._sizes = [100]
+    ax01.scatter(*electrode_data_mx, c="#0000FF", alpha=alpha, label="Max")
+    ax01.set_title("Max's")
 
-    axs[0, 1].scatter(*electrode_data_mx, c="#0000FF", alpha=alpha, label="Max")
-    axs[0, 1].set_title("Max's")
-
-    axs[1, 1].scatter(*electrode_data_tj, c="#FF0000", alpha=alpha, label="TJ")
-    axs[1, 1].set_title("TJ's")
+    ax11.scatter(*electrode_data_tj, c="#FF0000", alpha=alpha, label="TJ")
+    ax11.set_title("TJ's")
 
     spacing = 0.2
     plt.subplots_adjust(wspace=spacing, hspace=spacing)
